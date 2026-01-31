29043a73747ce9824ff94b5c475d854d825e87a4
tarutani tomoaki <syatk19840818gd@gmail.com>
2026-01-31 22:39:52 +0900

    コード変更

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def _set_background(image_path: str) -> None:
             font-family: "Yu Gothic", "游ゴシック", "YuGothic", "Hiragino Kaku Gothic ProN", "Meiryo", sans-serif;
             font-size: 16px;
             margin-top: 0px;
-            margin-bottom: 10px;
+            margin-bottom: 15px;
           }}
 
           /* 入力欄のフォント */
