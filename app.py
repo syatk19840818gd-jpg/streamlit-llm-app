b6e489a6b7831d1d30f6278f1f221a4d5544acef
tarutani tomoaki <syatk19840818gd@gmail.com>
2026-01-31 19:45:18 +0900

    変更完了

--- app.py
+++ app.py
@@ -417,7 +417,7 @@ st.markdown(f'<div class="wani-title">{APP_TITLE}</div>', unsafe_allow_html=True
 st.markdown(
     """
     <div class="wani-desc">
-      この世界のことを、な〜んでもしってるワニ先生だよ！！！！<br>
+      この世界のことを、な〜んでもしってるワニ先生だよ！<br>
       キミが「しりたいこと」を２つのモードから選んでね！
     </div>
     """,
